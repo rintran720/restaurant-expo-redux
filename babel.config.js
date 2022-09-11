9c4fb1dbbd14f3987ea40175af599d4384c1b473
johntranviact <john.tran@viact.ai>
2022-09-11 23:54:28 +0700

    feat: add lint and husky

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,16 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'babel-plugin-root-import',
+        {
+          root: __dirname,
+          rootPathPrefix: '~',
+          rootPathSuffix: './src',
+        },
+      ],
+    ],
   };
 };
